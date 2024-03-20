cb1394212ffdab8f8239107e55f2fa82ec6d6cc9
jatin1793 <jatinwakodikar@gmail.com>
2024-03-20 10:28:35 +0530

    done

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import authReducer from './reducers/authSlice.js'
 import { configureStore } from '@reduxjs/toolkit';
 // import { persistReducer } from "redux-persist";
-import { persistReducer } from 'redux-persist/dist/redux-persist.esm.js';
+import { persistReducer } from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
