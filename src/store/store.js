47d20dc84d9c8163a2e9ea3500064f6c0d0a83fb
jatin1793 <jatinwakodikar@gmail.com>
2024-03-20 15:25:07 +0530

    done

--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,7 @@
-// import authReducer from './reducers/authSlice.js'
-// import { configureStore } from '@reduxjs/toolkit';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, authReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-// });
-// const { configureStore } = require('@reduxjs/toolkit');
-// const { persistReducer } = require('redux-persist');
-// const storage = require('redux-persist/lib/storage').default;
-// const authReducer = require('./reducers/authSlice.js');
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, authReducer);
-
-// exports.store = configureStore({
-//   reducer: persistedReducer,
-// });
-
-const { configureStore } = require('@reduxjs/toolkit');
-const { persistReducer } = require('redux-persist');
-const storage = require('redux-persist/lib/storage').default;
-const authReducer = require('./reducers/authSlice.js');
+import authReducer from './reducers/authSlice.js'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
@@ -45,4 +14,4 @@ const store = configureStore({
   reducer: persistedReducer,
 });
 
-module.exports = { store };
+export default store;
